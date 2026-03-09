8d9d6ebd106054acd3c2d48d70a215e54a921e50
adriana1005 <adrianaabreu1005@gmail.com>
2026-03-09 17:18:40 +0000

    ola

--- apagar.py
+++ apagar.py
@@ -1,5 +1,22 @@
 import pandas as pd
+import re
+"""
 
+df = pd.read_csv("wikipedia_dataset.csv", encoding="utf-8")
+
+# conjunto para guardar caracteres encontrados
+caracteres_especiais = set()
+
+# percorrer colunas de texto
+for coluna in df.select_dtypes(include="object"):
+    for valor in df[coluna].dropna():
+        encontrados = re.findall(r"[^\w\s]", str(valor))
+        caracteres_especiais.update(encontrados)
+
+print("Caracteres especiais encontrados:")
+print(sorted(caracteres_especiais))
+
+"""
 df = pd.read_csv(
     "wikipedia_dataset.csv",
     sep=";",
@@ -8,7 +25,6 @@ df = pd.read_csv(
 )
 
 df = df.drop_duplicates()
-#rawr
 
 print(df.columns)
 
