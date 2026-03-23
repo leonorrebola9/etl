dedb5c35f15fa3f373a89319832600f23e66f335
adriana1005 <adrianaabreu1005@gmail.com>
2026-03-23 15:30:28 +0000

    tokens

---
+++ lab04.py
@@ -0,0 +1,25 @@
+import pandas as pd
+import nltk
+import string
+from nltk.corpus import stopwords
+from nltk.tokenize import word_tokenize
+
+# downloads necessários
+nltk.download('punkt')
+nltk.download('punkt_tab')
+nltk.download('stopwords')
+
+df = pd.read_csv("tweet_sub.csv")
+
+stop_words = set(stopwords.words('portuguese'))
+
+def preprocessar(texto):
+    texto = texto.lower()
+    texto = texto.translate(str.maketrans('', '', string.punctuation))
+    tokens = word_tokenize(texto)
+    tokens = [t for t in tokens if t not in stop_words]
+    return tokens
+
+df["tokens"] = df["tweet"].apply(preprocessar)
+
+print(df)
